74cc47d534854c1717cee070ea32a58b46cdf04c
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-11-17 15:54:36 +0100

    Enable edge to edge in MainActivity.kt

--- app/src/main/java/dev/hayohtee/dailoz/MainActivity.kt
+++ app/src/main/java/dev/hayohtee/dailoz/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.hayohtee.dailoz
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +12,7 @@ import dev.hayohtee.dailoz.ui.theme.DailozTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             DailozTheme {
