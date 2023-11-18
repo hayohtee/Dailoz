88e859a31b1332fbdeecf245a156d46a63419bba
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-11-18 14:30:50 +0100

    Create LoginViewModel.kt

---
+++ app/src/main/java/dev/hayohtee/dailoz/ui/screen/login/LoginViewModel.kt
@@ -0,0 +1,19 @@
+package dev.hayohtee.dailoz.ui.screen.login
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class LoginViewModel: ViewModel() {
+    private val _uiState = MutableStateFlow(LoginUiState())
+    val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
+
+    fun updateEmail(email: String) {
+        _uiState.value = _uiState.value.copy(email = email)
+    }
+
+    fun updatePassword(password: String) {
+        _uiState.value = _uiState.value.copy(password = password)
+    }
+}
