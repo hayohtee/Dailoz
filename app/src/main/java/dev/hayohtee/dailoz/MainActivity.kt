c69f98542ea20097a1a00f89dd9131902c84e893
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-11-22 10:09:24 +0100

    Annotate MainActivity.kt and inject MainViewModel

--- app/src/main/java/dev/hayohtee/dailoz/MainActivity.kt
+++ app/src/main/java/dev/hayohtee/dailoz/MainActivity.kt
@@ -8,9 +8,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import dev.hayohtee.dailoz.ui.MainViewModel
 import dev.hayohtee.dailoz.ui.theme.DailozTheme
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
