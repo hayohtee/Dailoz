9bfb1b37aacb3bf74fb5ea299f663a4bf1f3d112
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-11-22 09:33:46 +0100

    Add preference datastore dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
