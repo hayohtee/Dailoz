3698ba7b278768f1a05c6f97519b5c7b8795466b
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-11-29 19:04:25 +0100

    Create TaskDestination.kt

---
+++ app/src/main/java/dev/hayohtee/dailoz/ui/screen/task/TaskDestination.kt
@@ -0,0 +1,7 @@
+package dev.hayohtee.dailoz.ui.screen.task
+
+import dev.hayohtee.dailoz.ui.navigation.DailozDestination
+
+object TaskDestination : DailozDestination {
+    override val route: String = "task"
+}
