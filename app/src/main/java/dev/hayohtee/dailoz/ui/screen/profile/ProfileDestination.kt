e130d1a15edd77f60eaac648c1f62238d05d5efc
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-11-29 19:04:13 +0100

    Create ProfileDestination.kt

---
+++ app/src/main/java/dev/hayohtee/dailoz/ui/screen/profile/ProfileDestination.kt
@@ -0,0 +1,7 @@
+package dev.hayohtee.dailoz.ui.screen.profile
+
+import dev.hayohtee.dailoz.ui.navigation.DailozDestination
+
+object ProfileDestination: DailozDestination {
+    override val route: String = "profile"
+}
