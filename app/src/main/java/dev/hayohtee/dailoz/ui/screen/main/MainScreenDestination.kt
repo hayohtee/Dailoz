a60c217782a4bf956fce8662714f086722426372
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-11-29 20:03:21 +0100

    Create MainScreenDestination.kt

---
+++ app/src/main/java/dev/hayohtee/dailoz/ui/screen/main/MainScreenDestination.kt
@@ -0,0 +1,7 @@
+package dev.hayohtee.dailoz.ui.screen.main
+
+import dev.hayohtee.dailoz.ui.navigation.DailozDestination
+
+object MainScreenDestination: DailozDestination {
+    override val route: String = "mainScreen"
+}
