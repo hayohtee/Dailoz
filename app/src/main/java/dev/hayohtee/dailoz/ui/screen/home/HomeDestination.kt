2857b7e65a0b538dab74ffc3e25493984920a283
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-11-29 19:04:03 +0100

    Create HomeDestination.kt

---
+++ app/src/main/java/dev/hayohtee/dailoz/ui/screen/home/HomeDestination.kt
@@ -0,0 +1,7 @@
+package dev.hayohtee.dailoz.ui.screen.home
+
+import dev.hayohtee.dailoz.ui.navigation.DailozDestination
+
+object HomeDestination : DailozDestination {
+    override val route: String = "home"
+}
